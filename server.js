4da51f89f17ff7bed9a0210a517f0c618b73aa4e
WAKOYA_KUMSA\waqo1 <waqo1234567@gmail.com>
2024-07-18 09:55:31 +0300

    testing real time chat

--- server.js
+++ server.js
@@ -5,32 +5,25 @@ const socketio = require('socket.io');
 const formatMessage = require('./utils/messages');
 const WebSocket = require('ws');
 
-// Create a WebSocket server
+
 const wss = new WebSocket.Server({ port: 3000 });
 
-// Store connected clients
-const clients = new Set();
-
-// Handle connection event
 wss.on('connection', (ws) => {
-  // Add client to the set
-  clients.add(ws);
+  console.log('Client connected');
 
-  // Handle message event
   ws.on('message', (message) => {
+    console.log('Received message:', message.toString('utf8'));
+
     // Broadcast the received message to all connected clients
-    clients.forEach((client) => {
+    wss.clients.forEach((client) => {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
         client.send(message);
       }
     });
   });
 
-  // Handle close event
   ws.on('close', () => {
-    // Remove client from the set
-    clients.delete(ws);
+    console.log('Client disconnected');
   });
-});
 
-console.log('WebSocket server started on port 8080');
+});
